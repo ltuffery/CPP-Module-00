454f2efc1c151ce0f02938c47ba8e100221e7f16
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-05-19 15:39:28 +0200

    fix space in ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,19 +1,26 @@
+#include <cctype>
 #include <cstring>
 #include <iostream>
 
+char	toUpper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	else
+		return (c);
+}
+
 int	main(int ac, char **av)
 {
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
 	for (int i = 1; i < ac; i++) {
-		for (size_t j = 0; j < std::strlen(av[i]); j++)
-		{
-			if (av[i][j] >= 'a' && av[i][j] <= 'z')
-				std::cout << char(av[i][j] - 32);
-			else
-			 	std::cout << av[i][j];
-
+		for (size_t j = 0; j < std::strlen(av[i]); j++) {
+			std::cout << toUpper(av[i][j]);
 		}
-		if (i + 1 != ac)
-			std::cout << " ";
 	}
 	std::cout << std::endl;
 }
